8850541736308b4547112d3f1654a0e2ba14dbaa
CptFishHead <39932507+CptFishHead@users.noreply.github.com>
2022-05-30 23:18:17 +1000

    init rep

---
+++ printer/views.py
@@ -0,0 +1,8 @@
+import imp
+from multiprocessing.spawn import import_main_path
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Здесь будет выведен список обновлений")
